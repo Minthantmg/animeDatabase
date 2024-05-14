509581e014d9b7560053e0a7e8f5dfc2aaa8cd64
Minthantmg <minthant180@gmail.com>
2024-05-14 18:32:02 +0900

    added business logic

--- hooks/useAnime.js
+++ hooks/useAnime.js
@@ -1,5 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
-import {getAnimeList} from "../api/anime.js";
+import {getAnimeList, getMangaList, getTrendList} from "../api/anime.js";
 
 const useGetAnimeList = () => {
     return useQuery({
@@ -8,8 +8,24 @@ const useGetAnimeList = () => {
     })
 }
 
+const useGetMangaList = () => {
+    return useQuery({
+        queryKey: ['get', 'manga'],
+        queryFn: getMangaList
+    })
+}
+
+const useGetTrendAnimeList = () => {
+    return useQuery({
+        queryKey: ['get', 'trendAnime'],
+        queryFn: getTrendList
+    })
+}
+
 export const useAnime = () => {
     return {
         useGetAnimeList,
+        useGetMangaList,
+        useGetTrendAnimeList,
     }
 }
