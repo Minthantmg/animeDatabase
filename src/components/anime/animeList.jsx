a78cc56f924a11697f816af8e7326c42351e1095
Minthantmg <minthant180@gmail.com>
2024-05-14 18:30:37 +0900

    update ui

---
+++ src/components/anime/animeList.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {useAnime} from "../../../hooks/useAnime.js";
+import MainButton from "../MainButton.jsx";
+import placeHolder from "../../assets/placeholder.png"
+const AnimeList = () => {
+    const {useGetAnimeList} = useAnime()
+    const {data: anime, isSuccess, isError, isLoading} = useGetAnimeList()
+    return (
+        <div>
+            {isSuccess && (
+                <>
+                    <div className="mx-52 grid gap-y-10 md:pt-24 my-8">
+                        {anime.map((item) => (
+                            <div key={item.id} className="flex shadow-lg container">
+                                <div className="w-1/2">
+                                    <div className="flex">
+                                        <div className="w-1/3">
+                                            <img src={item.attributes.posterImage?.original} alt="cover"
+                                                 className="w-full rounded-r-lg pl-4 pt-4"/>
+                                        </div>
+                                        <div className="w-2/3 pl-4 pt-4">
+                                            <div className="font-bold text-2xl font-mono">
+                                                {item.attributes.titles.en_jp}
+                                            </div>
+                                            <div className="flex gap-1 text-sm text-gray-500 pt-2">
+                                                <span>Duration : </span>
+                                                <span>{item.attributes.startDate}</span>
+                                                <span>~</span>
+                                                <span>{item.attributes.endDate}</span>
+                                            </div>
+                                            <div className="flex gap-2 text-sm pt-2 text-gray-900">
+                                                <div>
+                                                    Status :
+                                                </div>
+                                                {item.attributes.status}
+                                            </div>
+                                            <div className="flex gap-2 pt-2 text-gray-900 text-sm">
+                                                <div>
+                                                    Total episodes :
+                                                </div>
+                                                {item.attributes.episodeCount}
+                                            </div>
+                                            <div className="flex gap-2 pt-2 text-gray-900 text-sm">
+                                                <div>
+                                                    Score :
+                                                </div>
+                                                <div className="flex gap-1">
+                                                    <div>
+                                                        {item.attributes.averageRating} / <span className="text-xs">100</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="flex gap-2 pt-2 text-gray-900 text-sm">
+                                                <div>
+                                                    Total view count :
+                                                </div>
+                                                <div className="flex gap-1">
+                                                    {item.attributes.userCount}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="line-clamp-4 px-4 pt-2 mb-4">
+                                        {item.attributes.description}
+                                    </div>
+                                    <div className="flex justify-end items-center pt-4">
+                                        <MainButton
+                                            color={`text-black`}
+                                            bg={`bg-white`}
+                                            text="view"
+                                            goTo="/anime"
+                                            hover={`hover:bg-yellow-400 hover:text-white`}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="w-1/2 rounded-r-lg image-container">
+                                    <img
+                                        src={item.attributes.coverImage?.original || placeHolder}
+                                        alt="cover"
+                                        className="w-full rounded-r-lg h-full"
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default AnimeList;
