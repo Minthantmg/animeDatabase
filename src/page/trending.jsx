24867fe25a495c856ac8d0e6abe600bc56c2e3a1
Minthantmg <minthant180@gmail.com>
2024-05-14 18:31:10 +0900

    update ui

--- src/page/trending.jsx
+++ src/page/trending.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TrendList from "../components/trend/trendList.jsx";
 
 const Trending = () => {
     return (
         <div>
-            trend
+            <TrendList />
         </div>
     );
 };
