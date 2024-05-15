669b7299b0427c227226eddbec6e40722ab35c8e
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-15 22:48:53 +0630

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Navbar from "./components/Nav/navbar.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
