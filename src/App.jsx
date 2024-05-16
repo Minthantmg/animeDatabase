e5bc6a3a485372e36ff8d5da59ded3267cfa2a3d
Minthantmg <minthant180@gmail.com>
2024-05-16 18:37:43 +0900

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Anime from "./page/anime.jsx";
 import Manga from "./page/manga.jsx";
 import Trending from "./page/trending.jsx";
+import Detail from "./page/detail.jsx";
+import MangaDetail from "./page/mangaDetail.jsx";
 
 const App = () => {
     return (
@@ -15,6 +17,8 @@ const App = () => {
                 <Route path="/anime" element={<Anime />} />
                 <Route path="/manga" element={<Manga />} />
                 <Route path="/trending" element={<Trending />} />
+                <Route path="/detail/:id" element={<Detail />} />
+                <Route path="/mangaDetail/:id" element={<MangaDetail />} />
             </Routes>
         </BrowserRouter>
     );
