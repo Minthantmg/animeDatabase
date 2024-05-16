e8c0bb3a06c9b3c424cae88220b32cfe9441c850
Minthantmg <minthant180@gmail.com>
2024-05-16 18:36:59 +0900

    added business logic

--- api/anime.js
+++ api/anime.js
@@ -26,3 +26,57 @@ export const getTrendList = async () => {
         throw e;
     }
 }
+
+export const getAnimeById = async (id) => {
+    try {
+        const res = await axiosInstance.get(`anime/${id}`)
+        return res.data.data;
+    } catch (e) {
+        throw e;
+    }
+}
+
+export const getMangaById = async (id) => {
+    try {
+        const res = await axiosInstance.get(`manga/${id}`)
+        return res.data.data;
+    } catch (e) {
+        throw e;
+    }
+}
+
+export const getCastingById = async (id) => {
+    try {
+        const res = await axiosInstance.get(`anime/${id}/castings`)
+        return res.data.data;
+    } catch (e) {
+        throw e;
+    }
+}
+
+export const getCharacterById = async (id) => {
+    try {
+        const res = await axiosInstance.get(`anime/${id}/characters`)
+        return res.data.data;
+    } catch (e) {
+        throw e;
+    }
+}
+
+export const getEpisodeById = async (id) => {
+    try {
+        const res = await axiosInstance.get(`anime/${id}/episodes`)
+        return res.data.data;
+    } catch (e) {
+        throw e;
+    }
+}
+
+export const getStreamById = async (id) => {
+    try {
+        const res = await axiosInstance.get(`anime/${id}/streaming-links`)
+        return res.data.data;
+    } catch (e) {
+        throw e;
+    }
+}
