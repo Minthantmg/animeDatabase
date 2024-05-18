9ad7bc8ebe5d98bcc0298d624c54226d6d35e4bb
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-18 23:11:58 +0630

    added tabs data

---
+++ src/components/detail/constant.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+export const tabs = [
+    {
+        label: 'Details',
+        content: 'This is the overview content for the first tab.',
+    },
+    {
+        label: 'Characters & Staff',
+        content: 'This is the details content for the second tab.',
+    },
+    {
+        label: 'Episodes',
+        content: 'This is the reviews content for the third tab.',
+    },
+    {
+        label: 'Stat',
+        content: 'This is the reviews content for the third tab.',
+    },
+];
