11a480dc45bf5d2bd4e3513fe983319371fb2a35
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-18 23:11:30 +0630

    update ui

---
+++ src/components/detail/tabs/characters.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Characters = () => {
+    return (
+        <div>
+            characters
+        </div>
+    );
+};
+
+export default Characters;
