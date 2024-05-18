32ed8c1a9de7eba2ee02abeed06871493cad6658
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-18 23:11:25 +0630

    update ui

---
+++ src/components/detail/tabs/details.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Details = () => {
+    return (
+        <div>
+            detail
+        </div>
+    );
+};
+
+export default Details;
