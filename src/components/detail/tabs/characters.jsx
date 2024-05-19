603badaef0a9155e1fddd38f10167fdf9a915ea7
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-19 23:44:43 +0630

    update ui

--- src/components/detail/tabs/characters.jsx
+++ src/components/detail/tabs/characters.jsx
@@ -1,10 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {useAnime} from "../../../../hooks/useAnime.js";
 
-const Characters = () => {
+// eslint-disable-next-line react/prop-types
+const Characters = ({id}) => {
+
+    const {useGetCharacterById} = useAnime()
+
+    const {data: character} = useGetCharacterById(id)
     return (
-        <div>
-            characters
+        <div className="text-sm">
+            <span className="font-bold">
+                Characters & Voice Actors
+            </span>
+            <div className="h-[1px] bg-black"></div>
+            <div>
+                {character?.map((character, index) => (
+                    <div key={index}>
+                        {character?.id}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
