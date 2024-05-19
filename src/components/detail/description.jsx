964a63cd9a69f067e447bac066a57dccaa926e8b
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-19 23:44:45 +0630

    update ui

--- src/components/detail/description.jsx
+++ src/components/detail/description.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React, {useState} from 'react';
 import {tabs} from "./constant.js";
+import Details from "./tabs/details.jsx";
+import Characters from "./tabs/characters.jsx";
+import Episodes from "./tabs/episodes.jsx";
+import Stat from "./tabs/stat.jsx";
 
 
-
-const Description = () => {
+// eslint-disable-next-line no-unused-vars,react/prop-types
+const Description = ({id}) => {
     const [activeTab, setActiveTab] = useState(0);
 
     const handleClick = (index) => {
@@ -12,23 +16,20 @@ const Description = () => {
     };
 
     return (
-        <div className="tabs">
-            <ul className="flex gap-4 mt-4">
+        <div className="tabs lg:p-2">
+            <ul className="flex mt-4 gap-4">
                 {tabs.map((tab, index) => (
                     <li key={index}
-                        className={`px-4 py-2 cursor-pointer ${activeTab === index ? 'bg-yellow-400 pt-2 mx-2 rounded-t-lg' : ''}`}
+                        className={`px-6 py-2 cursor-pointer border rounded-lg text-sm 
+                        ${activeTab === index ? 'border-yellow-400 text-yellow-400' : 'border-gray-300'}`}
                         onClick={() => handleClick(index)}>
                         {tab.label}
                     </li>
                 ))}
             </ul>
-            <div className="h-[1px] bg-black"></div>
-            <div className="tabs-content">
-                {tabs.map((tab, index) => (
-                    <div key={index} className={`${activeTab === index ? 'active' : 'hidden'}`}>
-                        {activeTab === index && tab.content} {/* Only render content if active */}
-                    </div>
-                ))}
+            <div className="lg:pt-4">
+                {activeTab === 0 ? <Details id={id}/> : activeTab === 1 ? <Characters id={id}/> : activeTab === 2 ? <Episodes/> :
+                    <Stat/>}
             </div>
         </div>
     );
