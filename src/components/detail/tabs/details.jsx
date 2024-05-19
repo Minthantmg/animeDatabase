dd9892f39f5697e6df2fe0a31640e085e70a662c
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-19 23:44:51 +0630

    update ui

--- src/components/detail/tabs/details.jsx
+++ src/components/detail/tabs/details.jsx
@@ -1,10 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {useAnime} from "../../../../hooks/useAnime.js";
 
-const Details = () => {
+// eslint-disable-next-line react/prop-types
+const Details = ({id}) => {
+    const {useGetAnimeById} = useAnime()
+    const {data: single} = useGetAnimeById(id)
     return (
-        <div>
-            detail
+        <div className="text-sm">
+            <div className="font-bold">
+                Synopsis
+            </div>
+            <div className="h-[1px] bg-black"></div>
+            {/* eslint-disable-next-line react/prop-types */}
+            <div>{single?.attributes.synopsis}</div>
+
+            <div className="font-bold lg:pt-10">
+                Description
+            </div>
+            <div className="h-[1px] bg-black"></div>
+            {/* eslint-disable-next-line react/prop-types */}
+            <div>{single?.attributes.description}</div>
         </div>
     );
 };
