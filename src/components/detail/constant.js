e582388ca4829ea4f763ace93033ed2d99ee8e3e
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-19 23:45:12 +0630

    added tab data

--- src/components/detail/constant.js
+++ src/components/detail/constant.js
@@ -2,18 +2,14 @@
 export const tabs = [
     {
         label: 'Details',
-        content: 'This is the overview content for the first tab.',
     },
     {
         label: 'Characters & Staff',
-        content: 'This is the details content for the second tab.',
     },
     {
         label: 'Episodes',
-        content: 'This is the reviews content for the third tab.',
     },
     {
         label: 'Stat',
-        content: 'This is the reviews content for the third tab.',
     },
 ];
