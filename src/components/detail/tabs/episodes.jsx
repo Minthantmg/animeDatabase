2c2d5c547366e227e8efc10dd0d6d86a7198f392
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-20 22:34:02 +0630

    update ui

--- src/components/detail/tabs/episodes.jsx
+++ src/components/detail/tabs/episodes.jsx
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {useAnime} from "../../../../hooks/useAnime.js";
 
-const Episodes = () => {
+// eslint-disable-next-line react/prop-types
+const Episodes = ({id}) => {
+    const {useGetEpisodes} = useAnime()
+    const {data:episode} = useGetEpisodes(id)
+    console.log(episode)
     return (
         <div>
             <span>Synopsis</span>
