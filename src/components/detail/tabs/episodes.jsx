2b65c4fe3ae960edbc1867254daa477e82bc7bb2
Minthantmg <minthantmg@gicjp.com>
2024-05-21 18:19:10 +0900

    update ui

--- src/components/detail/tabs/episodes.jsx
+++ src/components/detail/tabs/episodes.jsx
@@ -5,11 +5,48 @@ import {useAnime} from "../../../../hooks/useAnime.js";
 // eslint-disable-next-line react/prop-types
 const Episodes = ({id}) => {
     const {useGetEpisodes} = useAnime()
-    const {data:episode} = useGetEpisodes(id)
+    const {data: episode, isSuccess} = useGetEpisodes(id)
     console.log(episode)
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        return `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
     return (
         <div>
-            <span>Synopsis</span>
+            {isSuccess && (
+                <>
+                    <span>Episodes ({episode.length})</span>
+                    <div className="h-[1px] bg-black"></div>
+                    <div className="my-2">
+                        {episode.map((item, index) => (
+                            <div key={index} className="my-2">
+                                <div className="flex justify-between">
+                                    <div className="flex">
+                                        <div>
+                                            {index + 1}.
+                                        </div>
+                                        <div>
+                                            <div className="font-bold text-blue-500">
+                                                {item.attributes.titles.en_us}
+                                            </div>
+                                            <div className="text-xs mb-2">
+                                                {item.attributes.titles.ja_jp}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>Aired: <span className="text-sm">{formatDate(item.attributes.airdate)}</span></div>
+                                    </div>
+                                </div>
+                                <div className="h-[1px] bg-black"></div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
