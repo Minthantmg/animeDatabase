6ec555ee34a7595515a02e18638ddde9990b3c5a
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-22 20:55:58 +0630

    update ui

--- src/components/detail/tabs/stat.jsx
+++ src/components/detail/tabs/stat.jsx
@@ -1,11 +1,58 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import {useAnime} from "../../../../hooks/useAnime.js";
 
-const Stat = () => {
+
+// eslint-disable-next-line no-unused-vars,react/prop-types
+const Stat = ({id}) => {
+    const {useGetAnimeById} = useAnime()
+    const {data: stat, isSuccess} = useGetAnimeById(id)
+    console.log(stat)
+
+    function numberWithCommas(x) {
+        if (typeof x === 'number') {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        } else {
+            return x;
+        }
+    }
+
     return (
-        <div>
-            stat
-        </div>
+        <>
+            {isSuccess && (
+                <div className="text-sm">
+                    <div className="font-bold">
+                        Summary Stats
+                    </div>
+                    <div className="h-[1px] bg-black"></div>
+                    <div className="mt-1">
+                            <span
+                                className="font-bold">Watching</span>: <span>{numberWithCommas(stat.attributes.userCount)}</span>
+                    </div>
+                    <div className="mt-1">
+                        <span
+                            className="font-bold">Favorite</span>: <span>{numberWithCommas(stat.attributes.favoritesCount)}</span>
+                    </div>
+                    <div className="font-bold mt-4">
+                        Score Stats
+                    </div>
+                    <div className="h-[1px] bg-black"></div>
+                    <div className="mt-1">
+                        <span
+                            className="font-bold">Popularity Rank</span>: <span>{stat.attributes.popularityRank}</span>
+                    </div>
+                    <div className="mt-1">
+                        <span
+                            className="font-bold">Score</span>: <span>{stat.attributes.averageRating}</span>
+                    </div>
+                    <div className="mt-1">
+                        <span
+                            className="font-bold">Total Episodes</span>: <span>{stat.attributes.episodeCount}</span>
+                    </div>
+                </div>
+            )
+            }
+        </>
     );
 };
 
