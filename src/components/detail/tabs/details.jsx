da1ec97f8a23a49853d67b9c3d7f1b20d77b0304
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-05-22 20:55:42 +0630

    update ui

--- src/components/detail/tabs/details.jsx
+++ src/components/detail/tabs/details.jsx
@@ -1,27 +1,42 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import {useAnime} from "../../../../hooks/useAnime.js";
+import Youtube from "../youtube.jsx";
 
 // eslint-disable-next-line react/prop-types
 const Details = ({id}) => {
     const {useGetAnimeById} = useAnime()
-    const {data: single} = useGetAnimeById(id)
+    const {data: single, isSuccess} = useGetAnimeById(id)
     return (
-        <div className="text-sm">
-            <div className="font-bold">
-                Synopsis
-            </div>
-            <div className="h-[1px] bg-black"></div>
-            {/* eslint-disable-next-line react/prop-types */}
-            <div>{single?.attributes.synopsis}</div>
+        <>
+            {isSuccess && (
+                <div>
+                    <div className="text-sm">
+                        <div className="font-bold">
+                            Synopsis
+                        </div>
+                        <div className="h-[1px] bg-black"></div>
+                        {/* eslint-disable-next-line react/prop-types */}
+                        <div className="mt-3">{single?.attributes.synopsis}</div>
 
-            <div className="font-bold lg:pt-10">
-                Description
-            </div>
-            <div className="h-[1px] bg-black"></div>
-            {/* eslint-disable-next-line react/prop-types */}
-            <div>{single?.attributes.description}</div>
-        </div>
+                        <div className="font-bold lg:pt-10">
+                            Description
+                        </div>
+                        <div className="h-[1px] bg-black"></div>
+                        {/* eslint-disable-next-line react/prop-types */}
+                        <div className="mt-3">{single?.attributes.description}</div>
+                        <div className="font-bold lg:pt-10">
+                            Trailer
+                        </div>
+                        <div className="h-[1px] bg-black"></div>
+                        <div className="flex justify-start items-center my-4">
+                            <Youtube videoId={single.attributes.youtubeVideoId}/>
+                        </div>
+                    </div>
+                </div>
+            )
+            }
+        </>
     );
 };
 
